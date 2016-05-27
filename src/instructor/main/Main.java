433b28bbc8ca8ccb1ffad7ddbb299f092bc376db
Wyndell Bibera <wyndell.bibera@gmail.com>
2016-05-27 22:06:28 +0800

    Initial commit
    Signed-off-by: Wyndell Bibera <wyndell.bibera@gmail.com>

---
+++ src/instructor/main/Main.java
@@ -0,0 +1,10 @@
+package instructor.main;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // A simple hello application
+    String greeting = "Hello LBYIET1";
+    System.out.println(greeting);
+  }
+}
