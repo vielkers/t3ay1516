8a4d54aaf6b493540e834fae02dc867172371620
martinezmartina <martinaa.martinez@yahoo.com>
2016-05-31 20:03:31 +0800

    Ex2-martinez
    Signed-off-by: martinezmartina <martinaa.martinez@yahoo.com>

--- src/instructor/templates/Ex1.java
+++ src/instructor/templates/Ex1.java
@@ -4,12 +4,20 @@ public class Ex1 {
   public static void main(String[] args) {
 
     // Exercise 1.1
-    int n = Integer.parseInt(args[0]);
+    //int n = Integer.parseInt(args[0]);
     // Apply factorial to n
     // if n = 5, 5! = 120
+    int a1 =0, a2 = 1, a3, i;
+    System.out.printf(a1 + " " + a2);
+    for (i=2; i<9;i++) {
+      a3 = a1 + a2;
+      System.out.printf(" " + a3);
+      a1 = a2;
+      a2 = a3;
+    }
 
     // Exercise 1.2
-    int[] nums = {3, 5, 1, 35, 29, 22, 82, 86, 95, 72};
+    //int[] nums = {3, 5, 1, 35, 29, 22, 82, 86, 95, 72};
     // Print all the odd numbers inside the nums array
 
   }
