bb7d5799139a31eb8b7aca56bcab96ff5cdeff18
Anton Santos <aysantos.12@gmail.com>
2016-05-31 20:17:51 +0800

    Ex 2 Santos Fibonacci
    Signed-off-by: Anton Santos <aysantos.12@gmail.com>

--- src/instructor/Santos/Ex2.java
+++ src/instructor/Santos/Ex2.java
@@ -19,12 +19,14 @@ public class Ex2 {
         // 3. You may need a variable to (a) hold current sum or (b) change values of the x's
 
           int n1=0,n2=1,n3,i,count=5;
-          System.out.print(n1+" "+n2);//printing 0 and 1
+          System.out.println(n1);
+          System.out.println(n2);
+
 
           for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed
              {
               n3=n1+n2;
-              System.out.print(" "+n3);
+              System.out.println(n3);
               n1=n2;
               n2=n3;
     }
