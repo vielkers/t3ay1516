39102e219e9423b771f83121f31aade786ac59b0
Wyndell Bibera <wyndell.bibera@gmail.com>
2016-06-06 10:37:32 +0800

    Added circle class
    Signed-off-by: Wyndell Bibera <wyndell.bibera@gmail.com>

---
+++ src/instructor/lectures/Circle.java
@@ -0,0 +1,17 @@
+package instructor.lectures;
+
+public class Circle {
+  // attributes
+  float x;
+  float y;
+  float radius;
+
+  // methods
+  float getArea() {
+    return 3.14f * radius * radius;
+  }
+
+  float getCircumference() {
+    return 2f * 3.14f * radius;
+  }
+}
