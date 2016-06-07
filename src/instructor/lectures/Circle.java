a9ffc5961c8f395f04cfbd42a3ce7ccc7325c627
Wyndell Bibera <wyndell.bibera@gmail.com>
2016-06-07 13:47:33 +0800

    Completed Circle class code
    Signed-off-by: Wyndell Bibera <wyndell.bibera@gmail.com>

--- src/instructor/lectures/Circle.java
+++ src/instructor/lectures/Circle.java
@@ -2,16 +2,67 @@ package instructor.lectures;
 
 public class Circle {
   // attributes
-  float x;
-  float y;
-  float radius;
+  private float x;
+  private float y;
+  private float radius;
 
   // methods
-  float getArea() {
+  public float getArea() {
     return 3.14f * radius * radius;
   }
 
-  float getCircumference() {
+  public float getCircumference() {
     return 2f * 3.14f * radius;
   }
+
+  // constructors
+
+  // default
+  public Circle() {
+    this.x = 0f;
+    this.y = 0f;
+    this.radius = 2f;
+  }
+
+  // custom 1
+  public Circle(float x, float y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 1f;
+  }
+
+  // custom 2
+  public Circle(float x, float y, float radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+
+  // Setters and Getters
+  public float getX() {
+    return x;
+  }
+
+  public void setX(float x) {
+    this.x = x;
+  }
+
+  public float getY() {
+    return y;
+  }
+
+  public void setY(float y) {
+    this.y = y;
+  }
+
+  public float getRadius() {
+    return radius;
+  }
+
+  public void setRadius(float radius) {
+    this.radius = radius;
+  }
+
 }
+
+
