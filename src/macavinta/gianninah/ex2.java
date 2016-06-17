cc4960564ffc9f447f9ec51f8e0c04b67f4f9329
giannninahmm <gianninah.macavinta@yahoo.com>
2016-06-17 11:47:22 +0800

    commit message
    Signed-off-by: giannninahmm <gianninah.macavinta@yahoo.com>

---
+++ src/macavinta/gianninah/ex2.java
@@ -0,0 +1,19 @@
+package macavinta.gianninah;
+
+public class ex2 {
+    int x1 = 0, x2 = 1;
+    int xn = 0;
+    if (n <= 0) {
+        System.out.println("Invalid ");
+    } else if (n == 1) {
+        System.out.println("fibonacci(1) = " + 0);
+    } else if (n == 2) {
+        System.out.println("fibonacci(2) = " + 1);
+    } else {
+        for (int i = 3; i <= n; i++) {
+            xn = x1 + x2;
+            x1 = x2;
+            x2 = xn;
+        }
+        System.out.println("fibonacci(" + n + ") = " + xn);
+}
