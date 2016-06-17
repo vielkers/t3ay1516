635b073bd357601551b4cc882124f05340f8d27e
ysaibravo <ysaibravo@gmail.com>
2016-06-17 11:32:01 +0800

    Ex3
    Signed-off-by: ysaibravo <ysaibravo@gmail.com>

--- src/ysai/bravo/PRICE.java
+++ src/ysai/bravo/PRICE.java
@@ -6,12 +6,12 @@ package ysai.bravo;
 public class PRICE {
 
     public static void main(String[] args){
-        ITEM cp = new ITEM(1, "cellphone", 12345.6f, "gadget", "pieces");
+        ITEM cp = new ITEM(1, "phone", 12345.6f, "gadget", "pieces");
 
         PO cp1 = new PO(32131241, 412f);
 
         System.out.println("Item ID: " + cp.getItemid());
-        System.out.println("Item Desccription: " + cp.getItemdescription());
+        System.out.println("Item Description: " + cp.getItemdescription());
         System.out.println("Item Type: " + cp.getItemtype());
         System.out.println("Price: " + cp.getPrice());
         System.out.println("UOM: " + cp.getUOM());
