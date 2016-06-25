8a83eea7136b2075f0df705b0ca85603ea5cf0dd
Jason Lau <jason_lau@dlsu.edu.ph>
2016-06-25 10:27:55 +0800

    submission
    Signed-off-by: Jason Lau <jason_lau@dlsu.edu.ph>

--- src/jason/lau/MultiPO.java
+++ src/jason/lau/MultiPO.java
@@ -14,7 +14,7 @@ public class MultiPO {
         Items Surf = new Items(3, "Pack", "Home Care", 9.00f);
         Items Rexona = new Items(4, "Bottles", "Personal Care", 65.45f);
 
-        int [] sarray = {4,5,8,9};
+        float [] sarray = {4f,5f,8f,9f};
         String [] larray = {"Shampoo", "Bottles", "Home Care", "Personal Care"};
         Items [] harray = {Dove, LiptonTea, Surf, Rexona};
         PurchaseOrder Here = new PurchaseOrder(1, sarray, larray,harray);
@@ -25,9 +25,7 @@ public class MultiPO {
 
         for(int i = 0; i < 4; i++) {
 
-            System.out.println(Here.quan[i] + " ");
-            System.out.println(Here.uom[i] + " ");
-            System.out.println(Here.box[i].getitemdesc + Here.quan[i] * Here.box[i].getPrice());
+            System.out.println(Here.getQuan(i) + " ");
         }
 
 
