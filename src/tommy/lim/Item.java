4807521e5b158e5a4a22f06808af0a9fa4defbd5
vielkers <vielka_carvajal@dlsu.edu.ph>
2016-06-25 10:22:45 +0800

    Submission for Ex3
    Signed-off-by: vielkers <vielka_carvajal@dlsu.edu.ph>

---
+++ src/tommy/lim/Item.java
@@ -0,0 +1,60 @@
+package tommy.lim;
+
+/**
+ * Created by IE on 6/7/2016.
+ */
+public class Item {
+    private int itemid;
+    private String itemdescription;
+    private String itemtype;
+    private float price;
+    private String UOM;
+
+    public Item(int itemid, String itemdescription, float price, String itemtype, String UOM) {
+        this.itemid = itemid;
+        this.itemdescription = itemdescription;
+        this.price = price;
+        this.itemtype = itemtype;
+        this.UOM = UOM;
+    }
+
+    public String getUOM() {
+        return UOM;
+    }
+
+    public void setUOM(String UOM) {
+        this.UOM = UOM;
+    }
+
+    public int getItemid() {
+        return itemid;
+    }
+
+    public void setItemid(int itemid) {
+        this.itemid = itemid;
+    }
+
+    public String getItemdescription() {
+        return itemdescription;
+    }
+
+    public void setItemdescription(String itemdescription) {
+        this.itemdescription = itemdescription;
+    }
+
+    public String getItemtype() {
+        return itemtype;
+    }
+
+    public void setItemtype(String itemtype) {
+        this.itemtype = itemtype;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
