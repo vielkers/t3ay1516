aa0b3b16b187d7a77c0a9bdc6a57d47c4a3309d5
Matthew Reyes <reyesmatthew@gmail.com>
2016-06-25 09:59:36 +0800

    added item for ex3
    Signed-off-by: Matthew Reyes <reyesmatthew@gmail.com>

--- src/matthew/reyes/Item.java
+++ src/matthew/reyes/Item.java
@@ -4,8 +4,47 @@ package src.matthew.reyes;
  * Created by victorreyes on 07/06/2016.
  */
 public class Item {
-    float price
-    int itemID
-    String type
-    String description
-}
+    private int id;
+    private String itemDescription;
+    private String itemType;
+    private float price;
+
+    public Item () {
+        this.id = 9999;
+        this.itemDescription = "item";
+        this.itemType = "item";
+        this.price = 100.00f;
+    }
+
+    public Item(int id, String itemDescription, String itemType, float price) {
+        this.id = id;
+        this.itemDescription = itemDescription;
+        this.itemType = itemType;
+        this.price = price;
+    }
+    public int getID() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getItemDescription() {
+        return itemDescription;
+    }
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+    public String getItemType() {
+        return itemType;
+    }
+    public void setItemType(String itemtype) {
+        this.itemType = itemType;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    }
+
