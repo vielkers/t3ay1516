2bdd597af26335c03c941bbb9c1d959eebd3344c
Jason Lau <jason_lau@dlsu.edu.ph>
2016-06-28 19:15:48 +0800

    Exercise 4 Final
    Signed-off-by: Jason Lau <jason_lau@dlsu.edu.ph>

--- src/jason/lau/Ex4/MultiPO.java
+++ src/jason/lau/Ex4/MultiPO.java
@@ -11,13 +11,13 @@ public class MultiPO {
         float sum = 0;
         float total = 0;
 
-        Items Dove = new Items(1, "Shampoo", "Personal Care", 240.01f);
-        Items LiptonTea = new Items(2, "Bottles", "Drinks", 46.89f);
-        Items Surf = new Items(3, "Pack", "Home Care", 9.00f);
-        Items Rexona = new Items(4, "Bottles", "Personal Care", 65.45f);
+        Items Dove = new Items(1, "Dove", "Personal Care", 240.01f);
+        Items LiptonTea = new Items(2, "LiptonTea", "Drinks", 46.89f);
+        Items Surf = new Items(3, "Surf", "Home Care", 9.00f);
+        Items Rexona = new Items(4, "Rexona", "Personal Care", 65.45f);
 
         int [] sarray = {4,5,8,9};
-        String [] larray = {"Shampoo", "Bottles", "Home Care", "Personal Care"};
+        String [] larray = {"Bottles", "Box", "Bag", "Bottles"};
         Items [] harray = {Dove, LiptonTea, Surf, Rexona};
         PurchaseOrder2 Here = new PurchaseOrder2(1, sarray, larray,harray);
 
@@ -34,7 +34,7 @@ public class MultiPO {
             total = total + sum;
         }
 
-        System.out.println("------------------------------");
+        System.out.println("-----------------------------");
         System.out.println("Total Purchase: " + total);
     }
 }
