7f43a1bdf0fdfaaa6887eb97f641e7c17c36725d
ysaibravo <ysaibravo@gmail.com>
2016-06-28 16:32:24 +0800

    src/ysai/bravo/Company.java
    Signed-off-by: ysaibravo <ysaibravo@gmail.com>

---
+++ src/ysai/bravo/Company.java
@@ -0,0 +1,57 @@
+package ysai.bravo;
+
+/**
+ * Created by IE on 6/28/2016.
+ */
+public class Company {
+    private int id;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Company() {
+        this.id = 0;
+        this.name = "ABC Corporation";
+        this.address = "Taft Avenue, Manila, Philippines";
+        this.phone = "955-8685";
+    }
+
+    public Company(int id, String name, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
