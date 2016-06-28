398706dec2b7aca3c70c417919545b7dcb38f030
Matthew Reyes <reyesmatthew@gmail.com>
2016-06-28 19:36:49 +0800

    Submission Exercise 3
    Signed-off-by: Matthew Reyes <reyesmatthew@gmail.com>

---
+++ src/matthew/reyes/Exercise3/Item.java
@@ -0,0 +1,60 @@
+package src.matthew.reyes.Exercise3;
+
+/**
+ * Created by victorreyes on 25/06/2016.
+ */
+public class Item {
+    private int itemId;
+    private String itemDescription;
+    private String itemType;
+    private float price;
+
+    public Item() {
+        this.itemId = 1001;
+        this.itemDescription = "Palmolive";
+        this.itemType = "Shampoo";
+        this.price = 75.70f;
+    }
+
+    public Item(int itemId, String itemDescription, String itemType, float price) {
+        this.itemId = itemId;
+        this.itemDescription = itemDescription;
+        this.itemType = itemType;
+        this.price = price;
+
+
+
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
--- src/matthew/reyes/getTotalPOAmount.java
+++
@@ -1,8 +0,0 @@
-package src.matthew.reyes;
-
-/**
- * Created by victorreyes on 07/06/2016.
- */
-public class getTotalPOAmount {
-
-}
