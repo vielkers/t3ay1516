8c5ef7288d9b5c88e86450ddb8157fa522cc3014
Wyndell Bibera <wyndell.bibera@gmail.com>
2016-07-07 12:36:48 +0800

    Added TODO to correct errors
    Signed-off-by: Wyndell Bibera <wyndell.bibera@gmail.com>

--- src/ysai/bravo/PRICE.java
+++ src/ysai/bravo/PRICE.java
@@ -6,10 +6,14 @@ package ysai.bravo;
 public class PRICE {
 
     public static void main(String[] args){
-        ITEM cp = new ITEM(1, "phone", 12345.6f, "gadget", "pieces");
+        // TODO: No matching constructor in ITEM class
+        // ITEM cp = new ITEM(1, "phone", 12345.6f, "gadget", "pieces");
 
-        PO cp1 = new PO(32131241, 412f);
+        // TODO: No matching constructor in PO class
+        // PO cp1 = new PO(32131241, 412f);
 
+        // TODO: Fix cp and cp1 creation and uncomment the preceding lines
+        /*
         System.out.println("Item ID: " + cp.getItemid());
         System.out.println("Item Description: " + cp.getItemdescription());
         System.out.println("Item Type: " + cp.getItemtype());
@@ -18,6 +22,7 @@ public class PRICE {
 
         System.out.println("PO Number: " + cp1.getPonumber());
         System.out.println("Quantity: " + cp1.getQuantity());
+        */
 
 
     }
