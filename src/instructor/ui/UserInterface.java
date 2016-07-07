542f8ad0df230fa5cda1fbc6ef6673f0047fbb9e
Wyndell Bibera <wyndell.bibera@gmail.com>
2016-07-07 22:59:43 +0800

    Added modifications to UI
    Signed-off-by: Wyndell Bibera <wyndell.bibera@gmail.com>

--- src/instructor/ui/UserInterface.java
+++ src/instructor/ui/UserInterface.java
@@ -1,5 +1,7 @@
 package instructor.ui;
 
+import instructor.solutions.Ex1;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,21 +15,35 @@ public class UserInterface extends JFrame {
   private JLabel nLabel;
   private JTextField n;
 
-  public static void main(String[] args) {
-    JFrame frame = new JFrame("UserInterface");
-    frame.setContentPane(new UserInterface().mainPanel);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.pack();
-    frame.setVisible(true);
-  }
-
   public UserInterface() {
     super("Main Interface");
     button1.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
-        System.out.println("Value in textbox: " + n.getText());
+        Ex1 e1 = new Ex1();
+
+        int val = Integer.parseInt(n.getText());
+        if (val <= 0) {
+          System.out.println("Invalid number. Only positive nos. allowed.");
+        } else {
+          int currentProduct = 1;
+
+          while (val > 1) {
+            currentProduct = currentProduct * val;
+            val--;
+          }
+          JOptionPane.showMessageDialog(null, n.getText() + "! = " + currentProduct, "Value",
+              JOptionPane.PLAIN_MESSAGE);
+        }
       }
     });
+  }
+
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("User Interface Demo");
+    frame.setContentPane(new UserInterface().mainPanel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
   }
 }
