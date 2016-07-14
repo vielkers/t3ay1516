8c00e85acba3691ca981690f35275b007bcaeeb4
Ryna Bato <rynabato@gmail.com>
2016-07-14 20:38:21 +0800

    UI exercise
    Signed-off-by: Ryna Bato <rynabato@gmail.com>

---
+++ src/ryna/bato/STATIC/MAIN.java
@@ -0,0 +1,7 @@
+package ryna.bato.STATIC;
+
+/**
+ * Created by rynabato on 6/25/2016.
+ */
+public class MAIN {
+}
---
+++ src/ryna/bato/UIDemo.java
@@ -0,0 +1,7 @@
+package ryna.bato;
+
+/**
+ * Created by rynabato on 7/14/2016.
+ */
+public class UIDemo {
+}
