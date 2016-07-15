1da71d614b7f7a254bbbd5ee0e0a3fa236465101
giannninahmm <gianninah.macavinta@yahoo.com>
2016-07-15 10:12:05 +0800

    ex6
    Signed-off-by: giannninahmm <gianninah.macavinta@yahoo.com>

---
+++ src/macavinta/gianninah/ex6/Item.java
@@ -0,0 +1,49 @@
+package macavinta.gianninah.ex6;
+
+/**
+ * Created by IE on 7/15/2016.
+ */
+public class Item {private int id;
+    private String itemDescription;
+    private String itemType;
+    private float price;
+
+    public Item(int id, String itemDescription, String itemType, float price){
+        this.id = id;
+        this.itemDescription = itemDescription;
+        this.itemType = itemType;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
---
+++ src/macavinta/gianninah/ex6/TotalPrice.java
@@ -0,0 +1,60 @@
+package macavinta.gianninah.ex6;
+
+/**
+ * Created by IE on 7/15/2016.
+ */
+public class TotalPrice {
+    public static void main(String[] args) {
+
+        float totalPrice = 0;
+
+        Company company = new Company(1,"Company Name","Company Address","1234567890");
+
+        Item[] item = new Item[] {
+                new Item(1, "Item Description 1", "Item Type 1", 1),
+                new Item(2, "Item Description 2", "Item Type 2", 2)
+        };
+
+        float[] quantity = {1,2};
+        String[] UnitOfMeasure = {"Unit 1", "Unit 2"};
+
+        PurchaseOrder PO = new PurchaseOrder(1, item, quantity, UnitOfMeasure);
+
+        float discount = 10;
+        float vat = 12;
+
+        System.out.println("");
+        System.out.println("Purchase Order");
+        System.out.println("PO Number: " + PO.getPONumber());
+
+        System.out.println("");
+        System.out.println("Company Information");
+        System.out.println("Company ID: " + company.getId());
+        System.out.println("Company Name: " + company.getName());
+        System.out.println("Company Address: " + company.getAddress());
+        System.out.println("Company Phone Number: " + company.getPhoneNumber());
+
+        System.out.println("");
+        System.out.println("Item Information");
+        for(int i = 0; i < item.length ; i++){
+            System.out.println("");
+            System.out.println("Item Information");
+            System.out.println("Description: " + item[i].getItemDescription());
+            System.out.println("Quantity: " + PO.getQuantity()[i]);
+            System.out.println("Type: " + item[i].getItemType());
+            System.out.println("Price: " + item[i].getPrice());
+            System.out.println("Unit of Measure: " + PO.getUnitOfMeasure()[i]);
+        }
+
+        for(int i = 0; i < item.length ; i++){
+            totalPrice += item[i].getPrice() * PO.getQuantity()[i];
+        }
+
+        System.out.println("");
+        System.out.println("Subtotal = " + totalPrice);
+        System.out.println("Discount (" + discount + "%) = " + (totalPrice * discount / 100));
+        System.out.println("vat (" + vat + "%) = " + (totalPrice * vat / 100));
+        System.out.println("Total = " + (totalPrice - totalPrice * discount / 100 - totalPrice * vat / 100));
+    }
+
+}
