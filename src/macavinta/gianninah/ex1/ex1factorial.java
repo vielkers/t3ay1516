71a31591a4f55abbdfddbc49cba564dee160f8d3
Wyndell Bibera <wyndell.bibera@gmail.com>
2016-07-27 20:12:59 +0800

    Corrected few syntax errors under macavinta
    Signed-off-by: Wyndell Bibera <wyndell.bibera@gmail.com>

--- src/macavinta/gianninah/ex1/ex1factorial.java
+++ src/macavinta/gianninah/ex1/ex1factorial.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 /**
  * Created by IE on 7/26/2016.
  */
-public class ex1factorial {public static void main(String args []){
+public class ex1factorial {
+  public static void main(String args[]) {
     int n;
     int ans = 1;
 
@@ -14,18 +15,17 @@ public class ex1factorial {public static void main(String args []){
     System.out.println("Enter a number: ");
     n = x.nextInt();
 
-    if(n <= 0){
-        System.out.println("Invalid number! input positive numbers only");
-    }
-    else{
-        while(n != 1){
-            ans = ans * n;
-            //n--;
-            System.out.println(ans);
-        }
+    if (n <= 0) {
+      System.out.println("Invalid number! input positive numbers only");
+    } else {
+      while (n != 1) {
+        ans = ans * n;
+        //n--;
+        System.out.println(ans);
+      }
 
     }
 
-
+  }
 
 }
